a93f94872b0cd699a651b04d4aae76f7bcc220ce
Marta Iachini <iachinimarta@gmail.com>
2022-08-23 17:51:16 +0200

    Prova inserimento 2nd componente

---
+++ src/components/MyHero.vue
@@ -0,0 +1,19 @@
+<template>
+    <div> 
+        <img src="https://www.drupal.org/files/issues/header_1.png"/>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'MyHero'
+    }
+</script>
+
+<style scoped>
+    img {
+        width: 100%;
+        height: 250px;
+        object-fit: center;
+    }
+</style>
